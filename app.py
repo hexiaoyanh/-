c3fdbf5a07bf41cd6bb85bcab90d016a49e20106
Mikahe <heiaoyang1@gmail.com>
2021-09-14 15:14:39 +0000

    配置生成url

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 
 import requests as requests
 from flask import Flask, render_template, request, jsonify, g
-
+import random,string
 app = Flask(__name__)
 
 
@@ -11,6 +11,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.before_request
+def getData():
+    with open('./data.json', 'r', encoding='utf-8') as f:
+        g.data = json.loads(f.read())
+
+
+@app.after_request
+def setData(e):
+    with open('./data.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(g.data, ensure_ascii=False))
+    return e
+
+def random_char(y):
+    return ''.join(random.choice(string.ascii_letters) for x in range(y))
 
 @app.post('/getSub')
 def sub():
@@ -30,14 +45,27 @@ def sub():
                     s = "trojan://" + uuid + '@' + i['value'] + '?' + params
                 new_data.append(s)
             elif j.startswith("vmess"):
-                item = json.loads(str(base64.b64decode(j.split('://')[1]), 'utf-8'))
+                item = json.loads(
+                    str(base64.b64decode(j.split('://')[1]), 'utf-8'))
                 item['port'] = i['value'].split(':')[1]
                 item['add'] = i['value'].split(':')[0]
                 new_data.append(
                     "vmess://" + str(base64.b64encode(json.dumps(item, ensure_ascii=False).encode("utf-8")), "utf-8"))
     res = "\n".join(new_data)
     res = str(base64.b64encode(res.encode("utf-8")), "utf-8")
-    return {"code": 200, "data": res}
+    url = random_char(8)
+    g.data.append({
+        'url':url,
+        'value':res
+    })
+    return {"code": 200, "url": url}
+
+@app.errorhandler(405)
+def error_405():
+    return jsonify({
+        "code":405,
+        "msg" : "method not allowed"
+    })
 
 
 @app.errorhandler(500)
@@ -49,4 +77,4 @@ def error_500():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
