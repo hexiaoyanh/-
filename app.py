8e49e7632eb156f8804945e371645e547c51468d
Mikahe <heiaoyang@foxmail.com>
2021-09-24 17:00:05 +0800

    update

--- app.py
+++ app.py
@@ -33,26 +33,29 @@ def random_char(y):
 def get_data(data):
     new_data = []
     for i in data:
-        req = requests.get(i['key']).text
-        req = str(base64.b64decode(req), "utf-8").split('\n')
-        req = list(filter(lambda x: x != '', req))
-        for j in req:
-            if j.startswith("vless") or j.startswith('trojan'):
-                uuid = j.split('://')[1].split('@')[0]
-                params = j.split('://')[1].split('?')[1]
-                if j.startswith("vless"):
-                    s = "vless://" + uuid + '@' + i['value'] + '?' + params
-                else:
-                    s = "trojan://" + uuid + '@' + i['value'] + '?' + params
-                new_data.append(s)
-            elif j.startswith("vmess"):
-                item = json.loads(
-                    str(base64.b64decode(j.split('://')[1]), 'utf-8'))
-                item['port'] = i['value'].split(':')[1]
-                item['add'] = i['value'].split(':')[0]
-                new_data.append(
-                    "vmess://" + str(base64.b64encode(json.dumps(item, ensure_ascii=False).encode("utf-8")),
-                                     "utf-8"))
+        try:
+            req = requests.get(i['key']).text
+            req = str(base64.b64decode(req), "utf-8").split('\n')
+            req = list(filter(lambda x: x != '', req))
+            for j in req:
+                if j.startswith("vless") or j.startswith('trojan'):
+                    uuid = j.split('://')[1].split('@')[0]
+                    params = j.split('://')[1].split('?')[1]
+                    if j.startswith("vless"):
+                        s = "vless://" + uuid + '@' + i['value'] + '?' + params
+                    else:
+                        s = "trojan://" + uuid + '@' + i['value'] + '?' + params
+                    new_data.append(s)
+                elif j.startswith("vmess"):
+                    item = json.loads(
+                        str(base64.b64decode(j.split('://')[1]), 'utf-8'))
+                    item['port'] = i['value'].split(':')[1]
+                    item['add'] = i['value'].split(':')[0]
+                    new_data.append(
+                        "vmess://" + str(base64.b64encode(json.dumps(item, ensure_ascii=False).encode("utf-8")),
+                                         "utf-8"))
+        except Exception:
+            pass
     res = "\n".join(new_data)
     res = str(base64.b64encode(res.encode("utf-8")), "utf-8")
     url = random_char(8)
